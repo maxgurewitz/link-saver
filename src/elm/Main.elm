b59ac0041b061045579a95bf53000aaa5e3c77ce
maxgurewitz <maxthegeek1@gmail.com>
2016-11-22 00:15:07 -0800

    add html program

--- src/elm/Main.elm
+++ src/elm/Main.elm
@@ -1,7 +1,24 @@
 module Main exposing (..)
 
-import Html exposing (text)
+import Html exposing (text, program)
+
+
+init =
+    ( Maybe.Nothing, Cmd.none )
+
+
+view model =
+    text "Hello, World!"
+
+
+update msg model =
+    ( model, Cmd.none )
 
 
 main =
-    text "Hello, World!"
+    program
+        { init = init
+        , update = update
+        , subscriptions = always Sub.none
+        , view = view
+        }

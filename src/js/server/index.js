eeb117cde523e15a2d81f2027efb0ac7d0b92478
Max Gurewitz <maxthegeek1@gmail.com>
2017-08-19 00:16:21 -0700

    add router

--- src/js/server/index.js
+++ src/js/server/index.js
@@ -1,9 +1,15 @@
 const Koa = require('koa');
+const router = require('koa-router')();
 
 const app = new Koa();
 
-app.use(ctx => {
-  ctx.body = 'Hello Koa';
+
+router.get('/connect', async (ctx, next) => {
+    ctx.body = 'foo';
 });
+
+app
+  .use(router.routes())
+  .use(router.allowedMethods());
 
 app.listen(3001);

e02c6d73fce2f7c8aefbcb9a125d41b0fd0044c2
maxgurewitz <maxthegeek1@gmail.com>
2017-10-29 22:12:42 -0400

    implement not found and parsing errors

--- src/js/server/index.js
+++ src/js/server/index.js
@@ -14,13 +14,53 @@ async function start() {
 
   await pg.connect();
 
+  const actions = {
+    createUser(data, send) {
+    }
+  };
+
+  const ERRORS = {
+    DEFAULT: 0,
+    BAD_REQUEST: 1,
+    NOT_FOUND: 2
+  }
+
   const sockjsServer = sockjs.createServer({
     sockjs_url: 'http://cdn.jsdelivr.net/sockjs/1.0.1/sockjs.min.js'
   });
 
   sockjsServer.on('connection', conn => {
-    conn.on('data', message => {
-      conn.write(JSON.stringify({ baz: 'bar' }));
+    function send() {
+      conn.send(JSON.stringify(arguments));
+    }
+
+    conn.on('data', unparsed => {
+      let message;
+      try {
+        message = JSON.parse(unparsed);
+      } catch (e) {
+        return send({
+          type: 'error',
+          data: {
+            code: ERRORS.BAD_REQUEST,
+            message: `failed to parse json request: ${e.stack}`
+          }
+        });
+      }
+
+      const action = actions[message.type];
+
+      if (!action) {
+        send({
+          type: 'error',
+          data: {
+            code: ERRORS.NOT_FOUND,
+            message: `action for type '${message.type}' not found`
+          }
+        });
+      } else {
+        action(message.data, send);
+      }
     });
   });
 
